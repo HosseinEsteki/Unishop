e35e9c52a74d9d0ad8b67f69593ed54292c6e4f9
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-11 11:24:34 +0330

    Create Routes, Models, Migrations, Controllers and struct Views

---
+++ app/Http/Controllers/FaqController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Faq;
+use Illuminate\Http\Request;
+
+class FaqController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('account')->name('account.')->group(function () {
+    Route::get('login', [\App\Http\Controllers\Account\AccountController::class, 'login']);
+    Route::get('password-recovery', [\App\Http\Controllers\Account\AccountController::class, 'password_recovery']);
+    Route::resource('orders', \App\Http\Controllers\Account\OrderController::class);
+    Route::resource('wishlist', \App\Http\Controllers\Account\WishListController::class, ['only' => ['index', 'show', 'store', 'destroy']]);
+    Route::singleton('profile', \App\Http\Controllers\Account\ProfileController::class, ['except' => ['show']]);
+    Route::resource('address', \App\Http\Controllers\Account\UserAddressController::class, ['only' => ['edit', 'update']]);
+    Route::resource('tickets', \App\Http\Controllers\Account\TicketController::class);
+});
+Route::get('about-us', [\App\Http\Controllers\AboutController::class, 'index']);
+Route::get('contact-us', [\App\Http\Controllers\ContactController::class, 'index']);
+Route::resource('faqs', \App\Http\Controllers\FaqController::class, ['only' => ['index', 'store']]);
+Route::get('search', [\App\Http\Controllers\SearchController::class, 'index']);
+Route::resource('products', \App\Http\Controllers\ProductController::class, ['only' => ['index', 'show']]);
+Route::resource('categories', \App\Http\Controllers\CategoryController::class, ['only' => ['index', 'show']]);
+Route::resource('cart', \App\Http\Controllers\CartController::class, ['only' => ['index', 'store', 'destroy']]);
