24c39b2e8b46f5f3cb224b173b593a857220d259
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-20 22:50:47 +0330

    priority of add tables

--- database/migrations/2023_12_31_160938_create_categories_table.php
+++ database/migrations/2023_12_31_160938_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('address');
+            $table->foreignId('photo_id')->constrained()->noActionOnDelete();
             $table->timestamps();
         });
     }
