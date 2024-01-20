c9e4e1a4a8ff42a019265ce212f8e9c22d466ede
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-20 22:54:05 +0330

    add truncate Database after run seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->TruncateDatabase();
         \App\Models\User::factory(10)->create();
         $this->call([
             ProductSeeder::class
@@ -20,5 +24,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+    }
+
+    private function TruncateDatabase(): void
+    {
+        Artisan::call('migrate:fresh');
     }
 }
