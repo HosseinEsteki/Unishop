2961099d11cc2446da95e7fe22ffb921f07fe862
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-21 10:48:40 +0330

    add wishlist

--- app/Models/WishList.php
+++ app/Models/WishList.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class WishList extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = ['user_id', 'product_id'];
+    public $incrementing = false;
+    protected $fillable = [
+        'user_id',
+        'product_id'
+    ];
+
+    #region Boot
+    protected static function boot(): void
+    {
+        parent::creating(function (Wishlist $wishList) {
+            if (WishList::where('user_id', $wishList->user_id)->where('product_id', $wishList->product_id)->count() > 0) {
+                return false;
+            }
+//            $wishList->user_id=Auth::id();
+            return true;
+        });
+    }
+    #endregion
+    #region Relations
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    #endregion
 }
