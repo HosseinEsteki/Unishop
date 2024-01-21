acbd1e774a4d10df2f05f7c271fd13857f236f46
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-21 10:48:48 +0330

    add wishlist

---
+++ database/factories/WishListFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WishList>
+ */
+class WishListFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $products = Product::all('id')->pluck('id');
+        return [
+            'product_id' => $products->random()
+        ];
+    }
+}
