b3bff8dec1ddfa87ce51696d3b370910f6b54355
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-22 21:59:06 +0330

    add Factories to Seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cart;
 use App\Models\Category;
+use App\Models\Discount;
+use App\Models\Order;
+use App\Models\OrderProduct;
 use App\Models\Photo;
 use App\Models\Product;
 use App\Models\ProductDetail;
@@ -11,6 +15,7 @@ use App\Models\Tag;
 use App\Models\User;
 use App\Models\View;
 use App\Models\WishList;
+use Database\Factories\DiscountFactory;
 use Database\Factories\TagFactory;
 use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -23,10 +28,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-        Product::factory(5)->has(ProductDetail::factory())->create();
+        Product::factory(10)->has(ProductDetail::factory())->create();
 
-        WishList::factory(30)->create();
+        User::factory(10)->
+        has(Order::factory()->
+        has(OrderProduct::factory(5)))->
+        create();
+        Discount::factory(5)->hasAttached([1, 2, 3], [], 'products')->create();
 
         View::factory(20)->create();
         Rate::factory(20)->create();
@@ -34,5 +42,9 @@ class ProductSeeder extends Seeder
         Tag::factory(20)->create();
         Photo::factory(30)->create();
         Category::factory(15)->create();
+
+        WishList::factory(30)->create();
+
+        Cart::factory(10)->create();
     }
 }
