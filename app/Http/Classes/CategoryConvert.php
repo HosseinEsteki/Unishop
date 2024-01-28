c902ec1dca90359cdc245f645831bb5136ba9674
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-28 13:10:00 +0330

    create CategoryConvert for side menu

---
+++ app/Http/Classes/CategoryConvert.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Classes;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class CategoryConvert
+{
+    public static function categoriesToArray(Collection $categories): array
+    {
+        $newArray = [];
+        $parents = $categories->where('parent', '=', null);
+        $children = $categories->where('parent', '!=', null);
+        foreach ($parents as $parent) {
+            $newArray[] = [
+                'id' => $parent->id,
+                'name' => $parent->name,
+                'address' => $parent->address,
+                'photo_id' => $parent->photo_id,
+                'items' => $children->where('parent', '=', $parent->id)->toArray()
+            ];
+        }
+        return $newArray;
+    }
+}
