a82a7c35d5054fea372a7df26b297a3de638bfa6
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-28 13:10:37 +0330

    organize category menu

--- resources/views/components/off-canvas/category-menu.blade.php
+++ resources/views/components/off-canvas/category-menu.blade.php
@@ -8,69 +8,18 @@
     </a>
     <nav class="offcanvas-menu">
         <ul class="menu">
-            <li class="has-children"><span><a href="#">کفش های مردانه</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">کفش ورزشی</a></li>
-                    <li><a href="#">کوهنوردی</a></li>
-                    <li><a href="#">کفشهای پوتین</a></li>
-                    <li><a href="#">کفش تابستانه</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
-            <li class="has-children"><span><a href="#">کفشهای زنانه</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">سندل زنانه</a></li>
-                    <li><a href="#">تخت و اسلیم</a></li>
-                    <li><a href="#">کوهنوردی</a></li>
-                    <li><a href="#">پاشنه بلند</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
-            <li class="has-children"><span><a href="#">لباس های مردانه</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">تیشرت و بلوز</a></li>
-                    <li><a href="#">پالتو</a></li>
-                    <li><a href="#">ژاکت</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
-            <li class="has-children"><span><a href="#">لباس زنانه</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">پیراهن</a></li>
-                    <li><a href="#">تیشرت و تاپ</a></li>
-                    <li><a href="#">لباس زیر</a></li>
-                    <li><a href="#">سوییشرت</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
-            <li class="has-children"><span><a href="#">کفش کودکان</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">پوتین</a></li>
-                    <li><a href="#">سندل</a></li>
-                    <li><a href="#">کفش اسپرت</a></li>
-                    <li><a href="#">کوهنوردی</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
-            <li class="has-children"><span><a href="#">کیغ و چرم</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">کیف دستی</a></li>
-                    <li><a href="#">کوله پشتی</a></li>
-                    <li><a href="#">کیف تحصیل</a></li>
-                    <li><a href="#">کیف رسمی و اداری</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
-            <li class="has-children"><span><a href="#">دستبند و آویز</a><span class="sub-menu-toggle"></span></span>
-                <ul class="offcanvas-submenu">
-                    <li><a href="#">دستبند فلزی</a></li>
-                    <li><a href="#">سر بند</a></li>
-                    <li><a href="#">ساعت لوکس</a></li>
-                    <li><a href="#">گردنبند</a></li>
-                    <li><a href="#">گوشواره</a></li>
-                    <li><a href="#">نمایش همه</a></li>
-                </ul>
-            </li>
+            @foreach($categories as $category)
+                <li class="@isset($category['items']) has-children @endisset"><span><a
+                            href="#">{{$category['name']}}</a><span class="sub-menu-toggle"></span></span>
+                    @isset($category['items'])
+                        <ul class="offcanvas-submenu">
+                            @foreach($category['items'] as $item)
+                                <li><a href="#">{{$item['name']}}</a></li>
+                            @endforeach
+                        </ul>
+                    @endisset
+                </li>
+            @endforeach
         </ul>
     </nav>
 </div>
