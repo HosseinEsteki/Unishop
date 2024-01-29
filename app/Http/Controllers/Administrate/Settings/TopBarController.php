594c9b60d94aa5dca4fc63edc15985cfc695e9df
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-29 19:06:19 +0330

    create TopBar Table

---
+++ app/Http/Controllers/Administrate/Settings/TopBarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Administrate\Settings;
+
+use App\Http\Controllers\Controller;
+use App\Models\Settings\TopBar;
+use Illuminate\Http\Request;
+
+class TopBarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TopBar $topBar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TopBar $topBar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TopBar $topBar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TopBar $topBar)
+    {
+        //
+    }
+}
