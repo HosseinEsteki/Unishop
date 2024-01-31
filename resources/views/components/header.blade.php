73eab74ba8a8d0158c6e6c74ecb0fc543033a277
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-01-31 20:44:32 +0330

    complete cart

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -85,41 +85,38 @@
                         @endauth
                     </ul>
                 </div>
-                <div class="cart"><a href="cart.html"></a><i class="icon-bag"></i><span class="count">3</span><span
-                        class="subtotal">98300 تومان</span>
+                <div class="cart"><a href="{{route('cart.index')}}"></a><i class="icon-bag"></i><span
+                        class="count">{{$user->carts->count()}}</span><span
+                        class="subtotal">{{$user->cartsPrice}} تومان</span>
                     <div class="toolbar-dropdown">
-                        <div class="dropdown-product-item"><span class="dropdown-product-remove"><i
-                                    class="icon-cross"></i></span><a class="dropdown-product-thumb"
-                                                                     href="shop-single.html"><img
-                                    src="/unishop-theme/img/cart-dropdown/01.jpg" alt="Product"></a>
-                            <div class="dropdown-product-info"><a class="dropdown-product-title"
-                                                                  href="shop-single.html">کفش ورزشی مردانه</a><span
-                                    class="dropdown-product-details">54200 تومان</span> <span
-                                    class="dropdown-product-details"> - 2</span></div>
-                        </div>
-                        <div class="dropdown-product-item"><span class="dropdown-product-remove"><i
-                                    class="icon-cross"></i></span><a class="dropdown-product-thumb"
-                                                                     href="shop-single.html"><img
-                                    src="/unishop-theme/img/cart-dropdown/02.jpg" alt="Product"></a>
-                            <div class="dropdown-product-info"><a class="dropdown-product-title"
-                                                                  href="shop-single.html">کلاه بازی تنیس</a><span
-                                    class="dropdown-product-details">54200 تومان</span> <span
-                                    class="dropdown-product-details"> - 2</span></div>
-                        </div>
-                        <div class="dropdown-product-item"><span class="dropdown-product-remove"><i
-                                    class="icon-cross"></i></span><a class="dropdown-product-thumb"
-                                                                     href="shop-single.html"><img
-                                    src="/unishop-theme/img/cart-dropdown/03.jpg" alt="Product"></a>
-                            <div class="dropdown-product-info"><a class="dropdown-product-title"
-                                                                  href="shop-single.html">کیف زنانه چرم آبی</a><span
-                                    class="dropdown-product-details">54200 تومان</span> <span
-                                    class="dropdown-product-details"> - 2</span></div>
-                        </div>
+                        @foreach($user->carts as $cart)
+                            @php
+                                $product=$cart->product;
+                                $pageAddress=route('products.show',['product'=>$cart->product->slug])
+                            @endphp
+                            <div class="dropdown-product-item">
+                            <span class="dropdown-product-remove">
+                                <i class="icon-cross"></i>
+                            </span>
+
+                                <a class="dropdown-product-thumb" href="{{$pageAddress}}">
+                                    <img src="{{$product->photo}}" alt="Product">
+                                </a>
+                                <div class="dropdown-product-info">
+                                    <a class="dropdown-product-title"
+                                       href="{{$pageAddress}}">{{$product->name}}</a>
+                                    <span class="dropdown-product-details">{{$product->price}} تومان</span>
+                                    <span class="dropdown-product-details"> - {{$cart->amount}}</span>
+                                </div>
+                            </div>
+                        @endforeach
+
                         <div class="toolbar-dropdown-group">
-                            <div class="column"><a class="btn btn-sm btn-block btn-secondary" href="cart.html">نمایش
+                            <div class="column"><a class="btn btn-sm btn-block btn-secondary"
+                                                   href="{{route('cart.index')}}">نمایش
                                     سبد</a></div>
                             <div class="column"><a class="btn btn-sm btn-block btn-success"
-                                                   href="checkout-address.html">تسویه</a></div>
+                                                   href="{{route('account.orders.create')}}">تسویه</a></div>
                         </div>
                     </div>
                 </div>
