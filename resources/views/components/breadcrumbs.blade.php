6aa76f7c5774470f1f099b37973efa21db25df9e
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-02-06 18:34:53 +0330

    fix position of breadcrumbs view

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,9 +1,10 @@
 <div class="page-title">
     <div class="container">
-        <div class="column">
+        <div class="row">
+            <div class="col-4">
             <h1>{{$page->name}}</h1>
         </div>
-        <div class="column">
+            <div class="col-8">
             <ul class="breadcrumbs">
                 @foreach($breadcrumbs as $row)
                     <li><a href="{{route($row['route'])}}"> {{$row['name']}}</a></li>
@@ -11,6 +12,7 @@
                 @endforeach
                 <li>{{$page['name']}}</li>
             </ul>
+        </div>
         </div>
     </div>
 </div>
