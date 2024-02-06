a2ce3924e99941f11b37717687822c13d0cbd5fb
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-02-06 14:43:08 +0330

    Create Breadcrumbs Class for calculating page breadcrumbs

---
+++ app/Http/Classes/Breadcrumbs.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Classes;
+
+use App\Models\Page;
+use Illuminate\Database\Eloquent\Collection;
+
+class Breadcrumbs
+{
+
+    public static function Breadcrumbs(Page $page): array
+    {
+        $pages = Page::all();
+        $page = $page->toArray();
+        return self::makeBreadcrumbs(self::makeTree($pages, $page));
+    }
+
+    private static function makeBreadcrumbs(array $collection, $items = [])
+    {
+        if (array_key_exists(0, $collection)) {
+            if (array_key_exists('children', $collection[0])) {
+                $collection = $collection[0]['children'];
+                $items[] = [
+                    'id' => $collection['id'],
+                    'name' => $collection['name'],
+                    'route' => $collection['route'],
+                ];
+                return self::makeBreadcrumbs($collection, $items);
+            }
+        }
+        return $items;
+
+    }
+
+    private static function makeTree(Collection $collection, $page, $array = null): ?array
+    {
+        if ($page["parent"] == null) {
+            return $page;
+        }
+        $parent = $collection->where('id', '=', $page["parent"])->first()->toArray();
+        $holder = self::makeTree($collection, $parent, $array);
+        $array[] = ['children' => $parent + $holder];
+        return $array;
+    }
+
+}
