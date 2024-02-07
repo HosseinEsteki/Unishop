08182ad38901f94e29cd84a6473e595343fa6aa3
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-02-07 18:40:02 +0330

    complete footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,18 +5,29 @@
                 <!-- Contact Info-->
                 <section class="widget widget-light-skin">
                     <h3 class="widget-title">ارتباط با ما</h3>
-                    <p class="text-white">تلفن ({{Setting::getValue('owner')}} ) : <a class="navi-link-light"
-                                                                                      href="tel:{{Setting::getValue('phone')}}">{{Setting::getValue('phone')}}</a>
+                    @if(Setting::getValue('phone'))
+                        <p class="text-white">تلفن :
+                            <a class="navi-link-light"
+                               href="tel:{{Setting::getValue('phone')}}">{{Setting::getValue('phone')}}</a>
+                        </p>
+                    @endif
+                    @if(Setting::getValue('mail'))
+                        <p class="text-white">
+                            ایمیل : <a class="navi-link-light"
+                                       href="mailto:{{Setting::getValue('mail')}}"> {{Setting::getValue('mail')}}</a>
+                        </p>
+                    @endif
+                    @if(Setting::getValue(('developer url'))) @endif
+                    <p class="text-white">وبسایت :
+                        <a class="navi-link-light"
+                           href="{{Setting::getValue(('developer url'))}}">{{Setting::getValue(('developer url'))}}</a>
                     </p>
-                    <p class="text-white">ایمیل : <a class="navi-link-light"
-                                                     href="mailto:{{Setting::getValue('mail')}}"> {{Setting::getValue('mail')}}</a>
-                    </p>
-                    <p class="text-white">وبسایت : <a class="navi-link-light"
-                                                      href="{{Setting::getValue(('creator url'))}}">{{Setting::getValue(('creator url'))}}</a>
-                    </p>
-                    <p><a class="navi-link-light"
-                          href="{{Setting::getValue('creator url')}}">{{Setting::getValue('creator description')}}</a>
-                    </p>
+                    @if(Setting::getValue('developer description'))
+                        <p>
+                            <a class="navi-link-light"
+                               href="{{Setting::getValue('developer url')}}">{{Setting::getValue('developer description')}}</a>
+                        </p>
+                    @endif
                     @foreach($socialMedias as $socialMedia)
                         <a class="social-button shape-circle sb-{{$socialMedia->name}} sb-light-skin"
                            href="{{$socialMedia->address}}"><i
@@ -24,48 +35,31 @@
                     @endforeach
                 </section>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <!-- Mobile App Buttons-->
-                <section class="widget widget-light-skin">
-                    <h3 class="widget-title">دکمه‌های مارکت موبایل</h3>
-                    <a class="market-button apple-button mb-light-skin" href="#">
-                        <span class="mb-subtitle">دانلود از مارکت</span>
-                        <span class="mb-title">اپ استور</span>
-                    </a>
-                    <a class="market-button google-button mb-light-skin" href="#">
-                        <span class="mb-subtitle">دانلود از مارکت</span>
-                        <span class="mb-title">گوگل پلی</span></a>
-                    <a class="market-button windows-button mb-light-skin" href="#">
-                        <span class="mb-subtitle">دانلود از مارکت</span>
-                        <span class="mb-title">ویندوز استور</span>
-                    </a>
-                </section>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <!-- About Us-->
-                <section class="widget widget-links widget-light-skin">
-                    <h3 class="widget-title">درباره ما</h3>
-                    <ul>
-                        <li><a href="#">حرفه و مهارت</a></li>
-                        <li><a href="moderniz.ir">درباره مدرنیزز</a></li>
-                        <li><a href="#">بیشتر بدانید</a></li>
-                        <li><a href="#">خدمات و سرویسها</a></li>
-                        <li><a href="#">وبلاگ گروه</a></li>
-                        <li><a href="#">مزایای استفاده از این قالب</a></li>
-                    </ul>
-                </section>
-            </div>
+            @if($siteApps->count()>0)
+                <div class="col-lg-3 col-md-6">
+                    <!-- Mobile App Buttons-->
+
+                    <section class="widget widget-light-skin">
+                        <h3 class="widget-title">دکمه‌های مارکت موبایل</h3>
+                        @foreach($siteApps as $siteApp)
+                            <a class="market-button mb-light-skin" style='background-image: url("{{$siteApp->image}}")'
+                               href="{{$siteApp->link}}">
+                                <span class="mb-subtitle">دانلود از مارکت</span>
+                                <span class="mb-title">{{$siteApp->name}}</span>
+                            </a>
+                        @endforeach
+                    </section>
+                </div>
+            @endif
+
             <div class="col-lg-3 col-md-6">
                 <!-- Account / Shipping Info-->
                 <section class="widget widget-links widget-light-skin">
                     <h3 class="widget-title">مشخصات حساب و دسترسی شما</h3>
                     <ul>
-                        <li><a href="account-profile.html">پروفایل شما</a></li>
-                        <li><a href="account-profile.html">قیمت و حمل و نقل</a></li>
-                        <li><a href="account-profile.html">بازپرداخت و جایگزینی</a></li>
-                        <li><a href="account-profile.html">مالیات</a></li>
-                        <li><a href="account-address.html">اطلاعات تحویل</a></li>
-                        <li><a href="account-profile.html">برنامه های وابسته</a></li>
+                        <li><a href="{{route('account.profile')}}">پروفایل من</a></li>
+                        <li><a href="{{route('account.orders.index')}}">سفارشات من</a></li>
+                        <li><a href="{{route('transportation')}}">درباره حمل و نقل</a></li>
                     </ul>
                 </section>
             </div>
@@ -75,24 +69,23 @@
             <div class="col-md-7 padding-bottom-1x">
                 <!-- Payment Methods-->
                 <div class="margin-bottom-1x" style="max-width: 615px;"><img
-                        src="/unishop-theme/img/payment_methods.png"
+                        src="{{Setting::getValue('payment methods')}}"
                         alt="Payment Methods">
                 </div>
             </div>
             <div class="col-md-5 padding-bottom-1x">
                 <div class="margin-top-1x hidden-md-up"></div>
                 <!--Subscription-->
-                <form class="subscribe-form" action="http://moderniz.ir" method="post" target="_blank" novalidate>
+                <form class="subscribe-form" action="{{route('subscribe.store')}}" method="post" target="_blank"
+                      novalidate>
+                    @csrf
                     <div class="clearfix">
                         <div class="input-group input-light">
                             <input class="form-control" type="email" name="EMAIL"
                                    placeholder="ایمیل خود را وارد کنید ..."><span class="input-group-addon"><i
                                     class="icon-mail"></i></span>
                         </div>
-                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                            <input type="text" name="b_c7103e2c981361a6639545bd5_1194bb7544" tabindex="-1">
-                        </div>
+
                         <button class="btn btn-primary" type="submit"><i class="icon-check"></i></button>
                     </div>
                     <span class="form-text text-sm text-white opacity-50 text-right">برای عضویت در خبرنامه , دریافت تخفیف ها , آخرین اخبار ، و فروش های ویژه </span>
@@ -100,8 +93,9 @@
             </div>
         </div>
         <!-- Copyright-->
-        <p class="footer-copyright text-right">© کلیه حقوق محفوظ است , ایجاد شده توسط &nbsp;<i
-                class="icon-heart text-danger"></i><a href="http://moderniz.ir/" target="_blank"> &nbsp;محمد شجاع</a>
+        <p class="footer-copyright text-right">© کلیه حقوق محفوظ است, ایجاد شده توسط &nbsp;<i
+                class="icon-heart text-danger"></i><a href="{{Setting::getValue('developer url')}}"
+                                                      target="_blank"> {{Setting::getValue('developer')}}</a>
         </p>
     </div>
 </footer>
