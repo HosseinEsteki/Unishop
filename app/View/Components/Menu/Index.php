4a9754505edd9fb1fb6e81c853452f11b817985b
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-02-15 23:31:01 +0330

    complete index menu

--- app/View/Components/Menu/Index.php
+++ app/View/Components/Menu/Index.php
@@ -17,12 +17,41 @@ class Index extends Component
         //
     }
 
+    private function makeMenu($menus): ?string
+    {
+        $menuString = null;
+        foreach ($menus as $menu) {
+            $hasItems = $menu['items'] != [];
+            $isHead = $menu['parent'] == null;
+            if ($isHead || !$hasItems)
+                $menuString .= '<li>';
+            else
+                $menuString .= '<li class="has-children">';
+            if ($menu['category_id']) {
+                $href = route('categories.show', ['category' => $menu['category']['slug']]);
+                $title = $menu['category']['name'];
+            } else {
+                $href = $menu['page_url'];
+                $title = $menu['page_name'];
+            }
+            $menuString .= '<a href="' . $href . '"><span>' . $title . '</span></a>';
+            if ($hasItems) {
+                $menuString .= '<ul class="sub-menu">';
+                $menuString .= $this->makeMenu($menu['items']);
+                $menuString .= '</ul>';
+            }
+            $menuString .= '</li>';
+        }
+        return $menuString;
+    }
+
     /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
     {
         $menus = ConvertToArray::menusToArray(\App\Models\Menu::with('category')->orderBy('priority')->get());
-        return view('components.menu.index', compact('menus'));
+        $menuString = $this->makeMenu($menus);
+        return view('components.menu.index', compact('menuString'));
     }
 }
--- resources/views/components/menu/index.blade.php
+++ resources/views/components/menu/index.blade.php
@@ -1,38 +1,5 @@
 <nav class="site-menu">
     <ul>
-        @foreach($menus as $menu)
-            <li>
-                @if($menu['category_id'])
-                    <a href="{{route('categories.show',['category'=>$menu['category']['slug']])}}"><span>{{$menu['category']['name']}}</span></a>
-                @else
-                    <a href="{{$menu['page_url']}}"><span>{{$menu['page_name']}}</span></a>
-                @endif
-                @if($menu['items'])
-                    <ul class="sub-menu">
-                        @foreach($menu['items'] as $item)
-                            <li class="@if($item['items'])has-children @endif">
-                                @if($item['category_id'])
-                                    <a href="{{route('categories.show',['category'=>$item['category']['slug']])}}"><span>{{$item['category']['name']}}</span></a>
-                                @else
-                                    <a href="{{$item['page_url']}}"><span>{{$item['page_name']}}</span></a>
-                                @endif
-                                @if($item['items'])
-                                    <ul class="sub-menu">
-                                        @foreach($item['items'] as $child) @endforeach
-                                        <li>
-                                            @if($child['category_id'])
-                                                <a href="{{route('categories.show',['category'=>$child['category']['slug']])}}">{{$child['category']['name']}}</a>
-                                            @else
-                                                <a href="{{$child['page_url']}}"><span>{{$child['page_name']}}</span></a>
-                                            @endif
-                                        </li>
-                                    </ul>
-                                @endif
-                            </li>
-                        @endforeach
-                    </ul>
-                @endif
-            </li>
-        @endforeach
+        {!! $menuString !!}
     </ul>
 </nav>
