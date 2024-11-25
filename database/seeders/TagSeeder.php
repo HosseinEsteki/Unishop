cdd660562b492d48d45cb3f7f56cd47d25044ab3
HosseinEsteki <45480012+HosseinEsteki@users.noreply.github.com>
2024-11-25 17:35:06 +0330

    create TagSeeder

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $tags = Tag::factory(30)->create();
+    }
+}
